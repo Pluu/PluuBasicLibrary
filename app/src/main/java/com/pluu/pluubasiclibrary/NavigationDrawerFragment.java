158fd5433d7260e7069af7a02a8fb8be64fc64c0
Pluu <pluulove@gmail.com>
2014-11-20 00:09:15 +0900

    1. Material Design Temp

---
+++ app/src/main/java/com/pluu/pluubasiclibrary/NavigationDrawerFragment.java
@@ -0,0 +1,305 @@
+package com.pluu.pluubasiclibrary;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.text.Collator;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Fragment used for managing interactions for and presentation of a navigation drawer.
+ * See the <a href="https://developer.android.com/design/patterns/navigation-drawer.html#Interaction">
+ * design guidelines</a> for a complete explanation of the behaviors implemented here.
+ */
+public class NavigationDrawerFragment extends Fragment
+	implements AdapterView.OnItemClickListener {
+
+	/**
+	 * Remember the position of the selected item.
+	 */
+	private static final String STATE_SELECTED_POSITION = "selected_navigation_drawer_position";
+
+	/**
+	 * Per the design guidelines, you should show the drawer on launch until the user manually
+	 * expands it. This shared preference tracks this.
+	 */
+	private static final String PREF_USER_LEARNED_DRAWER = "navigation_drawer_learned";
+
+	/**
+	 * A pointer to the current callbacks instance (the Activity).
+	 */
+	private NavigationDrawerCallbacks mCallbacks;
+
+	private DrawerLayout mDrawerLayout;
+	private ListView mDrawerListView;
+	private View mFragmentContainerView;
+
+	private int mCurrentSelectedPosition = 0;
+	private boolean mFromSavedInstanceState;
+	private boolean mUserLearnedDrawer;
+
+	public NavigationDrawerFragment() {
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		// Read in the flag indicating whether or not the user has demonstrated awareness of the
+		// drawer. See PREF_USER_LEARNED_DRAWER for details.
+		SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getActivity());
+		mUserLearnedDrawer = sp.getBoolean(PREF_USER_LEARNED_DRAWER, false);
+
+		if (savedInstanceState != null) {
+			mCurrentSelectedPosition = savedInstanceState.getInt(STATE_SELECTED_POSITION);
+			mFromSavedInstanceState = true;
+		}
+
+		// Select either the default item (0) or the last selected item.
+		selectItem(mCurrentSelectedPosition);
+	}
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		// Indicate that this fragment would like to influence the set of actions in the action bar.
+		setHasOptionsMenu(true);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+							 Bundle savedInstanceState) {
+		View root = inflater.inflate(R.layout.fragment_navigation_drawer, container, false);
+
+		mDrawerListView = (ListView) root.findViewById(R.id.list_navigation_drawer);
+		mDrawerListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				selectItem(position);
+			}
+		});
+		mDrawerListView.setItemChecked(mCurrentSelectedPosition, true);
+
+		updateListView(null);
+
+		return root;
+	}
+
+	public boolean isDrawerOpen() {
+		return mDrawerLayout != null && mDrawerLayout.isDrawerOpen(mFragmentContainerView);
+	}
+
+	/**
+	 * Users of this fragment must call this method to set up the navigation drawer interactions.
+	 *
+	 * @param fragmentId   The android:id of this fragment in its activity's layout.
+	 * @param drawerLayout The DrawerLayout containing this fragment's UI.
+	 */
+	public void setUp(int fragmentId, DrawerLayout drawerLayout) {
+		mFragmentContainerView = getActivity().findViewById(fragmentId);
+		mDrawerLayout = drawerLayout;
+
+		mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+
+		// If the user hasn't 'learned' about the drawer, open it to introduce them to the drawer,
+		// per the navigation drawer design guidelines.
+		if (!mUserLearnedDrawer && !mFromSavedInstanceState) {
+			mDrawerLayout.openDrawer(mFragmentContainerView);
+		}
+	}
+
+	private void selectItem(int position) {
+		mCurrentSelectedPosition = position;
+		if (mDrawerListView != null) {
+			mDrawerListView.setItemChecked(position, true);
+		}
+		if (mDrawerLayout != null) {
+			mDrawerLayout.closeDrawer(mFragmentContainerView);
+		}
+		if (mCallbacks != null) {
+			// TODO
+//			mCallbacks.onNavigationDrawerItemSelected(position);
+		}
+	}
+
+	@Override
+	public void onAttach(Activity activity) {
+		super.onAttach(activity);
+		try {
+			mCallbacks = (NavigationDrawerCallbacks) activity;
+		} catch (ClassCastException e) {
+			throw new ClassCastException("Activity must implement NavigationDrawerCallbacks.");
+		}
+	}
+
+	@Override
+	public void onDetach() {
+		super.onDetach();
+		mCallbacks = null;
+	}
+
+	@Override
+	public void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		outState.putInt(STATE_SELECTED_POSITION, mCurrentSelectedPosition);
+	}
+
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+	}
+
+	@Override
+	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+		// If the drawer is open, show the global app actions in the action bar. See also
+		// showGlobalContextActionBar, which controls the top-left area of the action bar.
+		if (mDrawerLayout != null && isDrawerOpen()) {
+			inflater.inflate(R.menu.global, menu);
+		}
+		super.onCreateOptionsMenu(menu, inflater);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+//		if (item.getItemId() == R.id.action_example) {
+//			Toast.makeText(getActivity(), "Example action.", Toast.LENGTH_SHORT).show();
+//			return true;
+//		}
+
+		return super.onOptionsItemSelected(item);
+	}
+
+	/**
+	 * Callbacks interface that all activities using this fragment must implement.
+	 */
+	public static interface NavigationDrawerCallbacks {
+		/**
+		 * Called when an item in the navigation drawer is selected.
+		 */
+		void onNavigationDrawerItemSelected(Fragment fragment);
+	}
+
+	private void updateListView(String path) {
+		if (path == null) {
+			path = "";
+		}
+
+		mDrawerListView.setAdapter(new SimpleAdapter(getActivity(), getData(path),
+			android.R.layout.simple_list_item_1, new String[]{"title"},
+			new int[]{android.R.id.text1}));
+		mDrawerListView.setTextFilterEnabled(true);
+		mDrawerListView.setOnItemClickListener(this);
+	}
+
+	protected List<Map<String, Object>> getData(String prefix) {
+		List<Map<String, Object>> myData = new ArrayList<Map<String, Object>>();
+
+		String category = getString(R.string.MANIFEST_ACTIVITY_INTENT_CATEGORY_NAME);
+
+		Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
+		mainIntent.addCategory(category);
+
+		PackageManager pm = getActivity().getPackageManager();
+		List<ResolveInfo> list = pm.queryIntentActivities(mainIntent, 0);
+
+		if (null == list)
+			return myData;
+
+		String[] prefixPath;
+		String prefixWithSlash = prefix;
+
+		if (prefix.equals("")) {
+			prefixPath = null;
+		} else {
+			prefixPath = prefix.split("/");
+			prefixWithSlash = prefix + "/";
+		}
+
+		int len = list.size();
+
+		Map<String, Boolean> entries = new HashMap<String, Boolean>();
+
+		for (int i = 0; i < len; i++) {
+			ResolveInfo info = list.get(i);
+			CharSequence labelSeq = info.loadLabel(pm);
+			String label = labelSeq != null
+				? labelSeq.toString()
+				: info.activityInfo.name;
+
+			if (prefixWithSlash.length() == 0 || label.startsWith(prefixWithSlash)) {
+
+				String[] labelPath = label.split("/");
+
+				String nextLabel = prefixPath == null ? labelPath[0] : labelPath[prefixPath.length];
+
+				if ((prefixPath != null ? prefixPath.length : 0) == labelPath.length - 1) {
+					addItem(myData, nextLabel, null);
+				} else {
+					if (entries.get(nextLabel) == null) {
+						addItem(myData, nextLabel, null);
+						entries.put(nextLabel, true);
+					}
+				}
+			}
+		}
+
+		Collections.sort(myData, sDisplayNameComparator);
+
+		return myData;
+	}
+
+	private final static Comparator<Map<String, Object>> sDisplayNameComparator =
+		new Comparator<Map<String, Object>>() {
+			private final Collator collator = Collator.getInstance();
+
+			public int compare(Map<String, Object> map1, Map<String, Object> map2) {
+				return collator.compare(map1.get("title"), map2.get("title"));
+			}
+		};
+
+	protected void addItem(List<Map<String, Object>> data, String name, Intent intent) {
+		Map<String, Object> temp = new HashMap<String, Object>();
+		temp.put("title", name);
+//		temp.put("intent", intent);
+		data.add(temp);
+	}
+
+	@SuppressWarnings("unchecked")
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		Map<String, Object> map = (Map<String, Object>)l.getItemAtPosition(position);
+
+		Intent intent = (Intent) map.get("intent");
+		startActivity(intent);
+	}
+
+	@Override
+	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+		Map<String, Object> map = (Map<String, Object>)parent.getItemAtPosition(position);
+		String path = (String) map.get("title");
+		updateListView(path);
+	}
+
+}
